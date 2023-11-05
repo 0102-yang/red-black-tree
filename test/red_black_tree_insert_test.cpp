7522ed8ea5aca55de77389c90d1f3038a2a59002
0102-yang <1657375983@qq.com>
2023-11-05 14:51:02 +0800

    Add insert test.

--- test/red_black_tree_insert_test.cpp
+++ test/red_black_tree_insert_test.cpp
@@ -7,14 +7,31 @@ TEST(InsertTests, MultipleElementsInsertTest)
     rbt::RedBlackTree<int, int> tree;
     std::array arr{10, 85, 15, 70, 20, 60, 30, 50, 65, 80, 90, 40, 5, 55};
 
+    ASSERT_EQ(tree.IsEmpty(), true);
+
     // Insertion.
     for (const int& e : arr) {
         tree.Insert(e, e + 1);
-
-#ifdef DEBUG
         tree.PrintTree();
-#endif
     }
-
+    ASSERT_EQ(tree.IsEmpty(), false);
     ASSERT_EQ(tree.Size(), arr.size());
+
+    for (const int& e : arr) {
+        tree.Insert(e, e + 2);
+    }
+    ASSERT_EQ(tree.Size(), arr.size());
+
+    // Get value.
+    {
+        for (const int& e : arr) {
+            auto [value, flag] = tree.GetValue(e);
+            ASSERT_EQ(flag, true);
+            ASSERT_EQ(value, e + 1);
+        }
+    }
+    {
+        auto [value, flag] = tree.GetValue(88);
+        ASSERT_EQ(flag, false);
+    }
 }
