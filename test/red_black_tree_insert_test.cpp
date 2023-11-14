b28a42d04499af8e8bea491b5500a7737974e551
0102-yang <1657375983@qq.com>
2023-11-15 01:45:49 +0800

    Modify insert tests.

--- test/red_black_tree_insert_test.cpp
+++ test/red_black_tree_insert_test.cpp
@@ -2,21 +2,35 @@
 
 #include "red_black_tree.h"
 
+TEST(InsertTests, EmptyTest)
+{
+    rbt::RedBlackTree<int, int> tree;
+    ASSERT_EQ(tree.IsEmpty(), true);
+}
+
+TEST(InsertTests, SimpleInsertTest)
+{
+    rbt::RedBlackTree<int, int> tree;
+    ASSERT_EQ(tree.IsEmpty(), true);
+
+    tree.Insert(1, 1);
+    ASSERT_EQ(tree.IsEmpty(), false);
+    ASSERT_EQ(tree.Size(), 1);
+}
+
 TEST(InsertTests, MultipleElementsInsertTest)
 {
     rbt::RedBlackTree<int, int> tree;
-    std::array arr{10, 85, 15, 70, 20, 60, 30, 50, 65, 80, 90, 40, 5, 55};
-
-    ASSERT_EQ(tree.IsEmpty(), true);
+    std::array arr{10, 85, 15, 70, 20, 60, 30, 50, 65, 80, 90, 40, 5, 55, 45};
 
     // Insertion.
     for (const int& e : arr) {
         tree.Insert(e, e + 1);
         tree.PrintTree();
     }
-    ASSERT_EQ(tree.IsEmpty(), false);
     ASSERT_EQ(tree.Size(), arr.size());
 
+    // Duplicate key insertion.
     for (const int& e : arr) {
         tree.Insert(e, e + 2);
     }
