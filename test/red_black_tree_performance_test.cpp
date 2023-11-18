c21ede4e2991e0951f41afa8b9e19b2b8eb32751
0102-yang <1657375983@qq.com>
2023-11-18 18:56:56 +0800

    Add new performance test.

--- test/red_black_tree_performance_test.cpp
+++ test/red_black_tree_performance_test.cpp
@@ -5,34 +5,77 @@
 
 #include "red_black_tree.h"
 
-TEST(PerformanceTest, GetValueTest)
-{
-    std::map<int, int> m;
-    rbt::RedBlackTree<int, int> t;
+constexpr int iterate_time = 10000000;
 
+TEST(PerformanceTest, OrderedElementsInsertAndGetTest)
+{
     // std::map.
-    auto start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000000; ++i) {
-        m[i] = i * 2;
+    auto begin = std::chrono::steady_clock().now();
+    {
+        std::map<int, int> m;
+        for (int i = 0; i < iterate_time; ++i) {
+            m.insert({i, i});
+        }
+        for (int i = 0; i < iterate_time; ++i) {
+            auto it = m.find(i);
+        }
     }
-    for (int i = 0; i < 1000000; ++i) {
-        auto it = m.find(i);
-        std::cout << it->second << '\n';
-    }
-    auto end = std::chrono::steady_clock::now();
-    const std::chrono::duration<double> map_duration = end - start;
+    auto end = std::chrono::steady_clock().now();
+    const auto map_time = std::chrono::duration<double>(end - begin).count();
 
     // red-black-tree.
-    start = std::chrono::steady_clock::now();
-    for (int i = 0; i < 1000000; ++i) {
-        t.Insert(i, i * 2);
+    begin = std::chrono::steady_clock().now();
+    {
+        rbt::RedBlackTree<int, int> t;
+        for (int i = 0; i < iterate_time; ++i) {
+            t.Insert(i, i);
+        }
+        for (int i = 0; i < iterate_time; ++i) {
+            auto [it, flag] = t.GetValue(i);
+        }
     }
-    for (int i = 0; i < 1000000; ++i) {
-        auto [it, flag] = t.GetValue(i);
-        std::cout << it << '\n';
+    end = std::chrono::steady_clock().now();
+    const auto tree_time = std::chrono::duration<double>(end - begin).count();
+
+    std::cout << "Ordered elements: Map time is " << map_time << " second(s).\n";
+    std::cout << "Ordered elements: Tree time is " << tree_time << " second(s).\n";
+}
+
+TEST(PerformanceTest, RandomElementsInsertAndGetTest)
+{
+    std::mt19937 mt;
+    std::uniform_int_distribution dist(10000, 99999);
+
+    // std::map.
+    auto begin = std::chrono::steady_clock().now();
+    {
+        std::map<int, int> m;
+        for (int i = 0; i < iterate_time; ++i) {
+            int random_number = dist(mt);
+            m.insert({random_number, random_number});
+        }
+        for (int i = 0; i < iterate_time; ++i) {
+            auto it = m.find(i);
+        }
     }
-    end = std::chrono::steady_clock::now();
-    const std::chrono::duration<double> tree_duration = end - start;
-    std::cout << "Find 1000000 elements in std::map using " << map_duration.count() << " second(s)." << std::endl;
-    std::cout << "Find 1000000 elements in rbt::tree using " << tree_duration.count() << " second(s)." << std::endl;
+    auto end = std::chrono::steady_clock().now();
+    const auto map_time = std::chrono::duration<double>(end - begin).count();
+
+    // red-black-tree.
+    begin = std::chrono::steady_clock().now();
+    {
+        rbt::RedBlackTree<int, int> t;
+        for (int i = 0; i < iterate_time; ++i) {
+            int random_number = dist(mt);
+            t.Insert(random_number, random_number);
+        }
+        for (int i = 0; i < iterate_time; ++i) {
+            auto [it, flag] = t.GetValue(i);
+        }
+    }
+    end = std::chrono::steady_clock().now();
+    const auto tree_time = std::chrono::duration<double>(end - begin).count();
+
+    std::cout << "Random elements: Map time is " << map_time << " second(s).\n";
+    std::cout << "Random elements: Tree time is " << tree_time << " second(s).\n";
 }
