b2b2d642ac594560854d900a176381f26a314e4c
0102-yang <1657375983@qq.com>
2025-03-12 11:08:42 +0000

    Remove CMake configuration files and add xmake build system support.

---
+++ install_xmake.sh
@@ -0,0 +1,3 @@
+sudo wget https://xmake.io/shget.text -O - | bash
+sudo ln -s ~/.local/bin/xmake /usr/bin/xmake
+sudo ln -s ~/.local/bin/xrepo /usr/local/bin/xrepo
---
+++ xmake.lua
@@ -0,0 +1,66 @@
+set_project("CppTest")
+set_version("1.0.0")
+
+add_rules("mode.debug", "mode.release")
+set_languages("c++latest")
+set_toolchains("llvm")
+set_runtimes("c++_shared")
+
+add_requires("spdlog")
+add_requires("gtest")
+
+target("red-black-tree")
+  if is_mode("release") then
+    set_optimize("fastest")
+    set_symbols("hidden")
+  end
+  if is_mode("debug") then
+    set_symbols("debug")
+    set_optimize("none")
+  end
+
+  set_kind("static")
+  add_includedirs("src", {public = true})
+  add_files("src/red_black_tree.cpp")
+  add_packages("spdlog")
+target_end()
+
+target("insert-test")
+  if is_mode("debug") then
+    set_symbols("debug")
+    set_optimize("none")
+  end
+
+  set_kind("binary")
+  add_files("test/test_main.cpp")
+  add_files("test/red_black_tree_insert_test.cpp")
+  add_deps("red-black-tree")
+  add_packages("gtest")
+  add_packages("spdlog")
+target_end()
+
+target("delete-test")
+  if is_mode("debug") then
+    set_symbols("debug")
+    set_optimize("none")
+  end
+
+  set_kind("binary")
+  add_files("test/test_main.cpp")
+  add_files("test/red_black_tree_delete_test.cpp")
+  add_deps("red-black-tree")
+  add_packages("gtest")
+  add_packages("spdlog")
+target_end()
+
+target("bench-mark")
+  if is_mode("debug") then
+    print("Warning: Bench-marking in debug mode is not optimized and recommended.")
+  end
+  
+  set_symbols("hidden")
+  set_optimize("fastest")
+  set_kind("binary")
+  add_files("bench/performance.cpp")
+  add_deps("red-black-tree")
+target_end()
